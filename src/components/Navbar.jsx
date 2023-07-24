86fcc4f0c556af698ef7ab61cea0faf6d6526262
Aniruddha <aniruddhavikharankarfb@gmail.com>
2023-07-24 13:56:15 +0530

    added about

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
                         <li>
-                            <Link href="/About" className="block py-2 pl-3 pr-4 text-gray-500 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">About</Link>
+                            <Link href="/About" className="block py-2 pl-3 pr-4 text-gray-500 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">About Us</Link>
                         </li>
                         <li>
                             <Link href="/BuyTicket" className="block py-2 pl-3 pr-4 text-gray-500 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Buy Flight Tickets</Link>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className="about pb-100 pt-[200px]">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-lg-6">
+                        <div className="about-img">
+                            <img src="../tick.webp" alt="image" />
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="about-text-area pl-20">
+                            <div className="default-section-title">
+                                <h6>ABOUT US</h6>
+                                <h3>Reckets as a resell ticketing platform</h3>
+                            </div>
+                            <p>We as a platform provide services to users where they can resell their online tickets if they are not able to use or cancel them. We charge less than 1% comission and guarntee a successful transfer between the buyer and a seller. </p>
+                            <div className="about-list">
+                                <ul>
+                                    <li>Resell Flight Tickets</li>
+                                    <li>Resell Boat tickets (Coming Soon..)</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
